1af1001e463993b4e9c9791169192e2aefa5b037
Jean Jonethal <jjonethal@gmail.com>
2026-04-29 22:39:55 +0200

    update pip and check tool installation

--- init.bat
+++ init.bat
@@ -2,7 +2,7 @@
 setlocal
 
 :: Check if virtual environment exists
-if not exist qrenv\Scripts\activate.bat call :init_env
+if not exist qrenv\Scripts\activate.bat call :init_env || ( echo error installing python environment )
 
 :: Activate virtual environment
 call qrenv\Scripts\activate.bat
@@ -17,5 +17,6 @@ echo Creating virtual environment...
 python -m venv qrenv || ( echo Error: Failed to create python environment & exit /b 1 )
 echo Installing dependencies...
 call qrenv\Scripts\activate.bat
+python -m pip install  -U pip || ( echo error updating pip & exit /b 1 )
 pip install -r requirements.txt
 exit /b 0
